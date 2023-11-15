b43c74eb5cff23e4b4465267973a5b383b6558cc
tyler :3 <tyler@shrapnelnet.co.uk>
2023-11-15 22:27:58 +0000

    add keep-alive

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false
+    reactStrictMode: false,
+    httpAgentOptions: {
+        keepAlive: true
+    }
 }
 
 module.exports = nextConfig
