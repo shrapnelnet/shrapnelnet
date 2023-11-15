f3f33e4ad36ecf179d471c5239074dcaa12f3bd6
tyler :3 <tyler@shrapnelnet.co.uk>
2023-11-15 21:16:58 +0000

    add loading wheel to pagination button

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Post from "@/components/Post"
 import PostSkeleton from "@/components/PostSkeleton"
 import IsLoggedIn from "@/api/IsLoggedIn"
 import {useEffect, useState} from "react"
-import { Box, Button, Typography } from "@mui/material"
+import { Box, Button, Typography, CircularProgress } from "@mui/material"
 import GetGlobalFeed from "@/api/GetGlobalFeed"
 
 export default function Index() {
@@ -14,6 +14,7 @@ export default function Index() {
     const [posts, setPosts] = useState([])
     const [showButton, setShowButton] = useState(true)
     const [endOfPosts, setEndOfPosts] = useState(false)
+    const [showLoading, setShowLoading] = useState(false)
 
     useEffect(() => {
         IsLoggedIn()
@@ -26,6 +27,7 @@ export default function Index() {
     }, [])
 
     const handleLoadMorePosts = () => {
+        setShowLoading(true)
         GetGlobalFeed(postOffset)
             .then((res) => {
                 if (res.length < 10) {
@@ -34,6 +36,7 @@ export default function Index() {
                 }
                 setPostOffset(postOffset + 10)
                 setPosts([...posts, ...res])
+                setShowLoading(false)
             })
     }
 
@@ -59,7 +62,13 @@ export default function Index() {
                                         null
                                 }
                                 <Box sx={{ display: "flex", justifyContent: "center", margin: "30px 0" }}>
-                                    <Button sx={{ display: showButton ? "block" : "none" }} onClick={handleLoadMorePosts} variant="contained">Load More</Button>
+                                    <Button sx={{ display: showButton ? "block" : "none", marginRight: "20px" }} onClick={handleLoadMorePosts} variant="contained">Load More</Button>
+                                    {
+                                        showLoading ?
+                                            <CircularProgress />
+                                            :
+                                            null
+                                    }
                                 </Box>
                                 <Typography display={endOfPosts ? "block" : "none"} sx={{ textAlign: "center", margin: "40px" }}>Nowhere but up from here...</Typography>
                             </>
